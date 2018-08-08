ebbc78f9f599b3cc00aded562a9268b7a4f0b1e2
yuusukesaito <ga23437@ga3.so-net.jp>
2018-08-08 17:39:44 +0900

    再提出

---
+++ Instagram/SettingViewController.swift
@@ -0,0 +1,79 @@
+//
+//  SettingViewController.swift
+//  Instagram
+//
+//  Created by 理化学Mac on 2018/07/28.
+//  Copyright © 2018年 yuusukesaito. All rights reserved.
+//
+
+import UIKit
+import ESTabBarController
+import Firebase
+import FirebaseAuth
+import SVProgressHUD
+
+class SettingViewController: UIViewController {
+    
+    @IBOutlet weak var displayNameTextField: UITextField!
+    
+    @IBAction func handleChangeButton(_ sender: Any) {
+        if let displayName = displayNameTextField.text{
+            if  displayName.isEmpty {
+                SVProgressHUD.showError(withStatus: "表示名を入力して下さい")
+                return
+            }
+            let user = Auth.auth().currentUser
+            if let user = user {
+                let changeRequest = user.createProfileChangeRequest()
+                changeRequest.displayName = displayName
+                changeRequest.commitChanges { error in
+                    if let error = error {
+                        SVProgressHUD.showError(withStatus: "表示名の変更に失敗しました")
+                        print("DEBUG_PRINT: " + error.localizedDescription)
+                        return
+                    }
+                    print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました")
+                    SVProgressHUD.showSuccess(withStatus: "表示名を変更しました")
+                }
+            }
+        }
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func handleLogoutButton(_ sender: Any) {
+        try! Auth.auth().signOut()
+        let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+        self.present(loginViewController!, animated: true, completion: nil)
+        let tabBarController = parent as! ESTabBarController
+        tabBarController.setSelectedIndex(0, animated: false)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let user = Auth.auth().currentUser
+        if let user = user {
+            displayNameTextField.text = user.displayName
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.4'
+
+target 'Instagram' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Instagram
+  pod 'ESTabBarController','0.2'
+  pod 'Firebase','5.3.0'
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'SVProgressHUD','2.2.5'
+  pod 'CLImageEditor/AllTools','0.2.3'
+
+end
